564ba5d603a983142127d9ba1f4bafdcbb490d1e
Nikola Bojanovic <nicolas_beograd@ymail.com>
2020-07-25 09:46:37 +0200

    temp test route using auth

--- server/src/routes/test.route.js
+++ server/src/routes/test.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { testController } = require('../controllers');
+const authorization = require('../util/authorization');
 const app = express();
 
 app.get('/', testController);
+app.post('/', authorization(), testController);
 
 module.exports = app;
