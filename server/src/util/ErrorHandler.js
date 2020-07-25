e1c5fec596b76ce0150a189461612181f9718a55
Nikola Bojanovic <nicolas_beograd@ymail.com>
2020-07-25 09:46:14 +0200

    Add default params to the ErrorHandler

--- server/src/util/ErrorHandler.js
+++ server/src/util/ErrorHandler.js
@@ -1,5 +1,5 @@
 class ErrorHandler extends Error {
-  constructor(statusCode, message) {
+  constructor(statusCode = 500, message = 'Internal Server Error') {
     super();
     this.statusCode = statusCode;
     this.message = message;
