e77c20857899f718309348da07328d60a468c757
jovan6jovan <jovan6jovanovic@gmail.com>
2020-07-30 11:54:44 +0200

    Make a wildcard route

--- ui/src/components/Routes/Routes.js
+++ ui/src/components/Routes/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "../Layout/Layout";
 // import Quizzes from "../Quizzes/Quizzes";
@@ -10,6 +10,7 @@ const Routes = () => (
     <Route exact path="/" component={Layout} />
     {/* <Route path="/quizzes" component={Quizzes} /> */}
     {/* <Route path="/ladder" component={Ladder} /> */}
+    <Route render={() => <Redirect to="/" />} />
   </Switch>
 );
 
--- ui/src/components/UserLoggedIn/UserLoggedIn.js
+++ ui/src/components/UserLoggedIn/UserLoggedIn.js
@@ -3,13 +3,14 @@ import "./UserLoggedIn.css";
 import iconPower from "../../assets/icon-power.svg";
 
 const UserLoggedIn = () => {
-  const [ isLoggedIn ] = useState(false);
+  const [isLoggedIn] = useState(false);
 
-  return isLoggedIn ? (
+  return isLoggedIn && (
     <div className="UserLoggedIn">
-      Welcome, <span className="UserLoggedIn-email">johnDoe@email.com</span> <img src={iconPower} className="UserLoggedIn-icon" alt="power icon" />
+      Welcome, <span className="UserLoggedIn-email">johnDoe@email.com</span>{" "}
+      <img src={iconPower} className="UserLoggedIn-icon" alt="power icon" />
     </div>
-  ) : null;
+  );
 };
 
 export default UserLoggedIn;
