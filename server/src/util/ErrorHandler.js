8c95e32c755fd95a0324b18212aad1d449fa1aa5
Lazar Karic <kadzajr@gmail.com>
2020-07-31 17:03:58 +0200

    Added default statusCode to constructor

--- server/src/util/ErrorHandler.js
+++ server/src/util/ErrorHandler.js
@@ -9,26 +9,17 @@ class ErrorHandler extends Error {
 }
 
 const handleError = (err, res) => {
-  // Dodato jer ValidationError nema statusCode, pa puca u default slucaju.
-  if (err instanceof Error.ValidationError) {
-    res.status(400).json({
-      status: "error",
-      statusCode: 400,
-      message: err.errors,
-    });
-  } else {
-    const { statusCode, message } = err;
+  const { statusCode = 400, message } = err;
 
-    if (process.env.NODE_ENV !== "production") {
-      console.log("\x1b[31m%s\x1b[0m", err.stack);
-    }
-
-    res.status(statusCode).json({
-      status: "error",
-      statusCode,
-      message,
-    });
+  if (process.env.NODE_ENV !== "production") {
+    console.log("\x1b[31m%s\x1b[0m", err.stack);
   }
+
+  res.status(statusCode).json({
+    status: "error",
+    statusCode,
+    message,
+  });
 };
 
 module.exports = {
