342557bf00c80145012cd3f5fd37c3a7d1c98daa
Lazar Karic <kadzajr@gmail.com>
2020-07-31 13:49:46 +0200

    Added register controller method

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,24 @@
+const { ErrorHandler, handleError } = require("../util/ErrorHandler");
+const User = require("../models/user.model");
+const _ = require("lodash");
+
+const register = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = new User({
+      email: email,
+      password: password,
+    });
+
+    const response = await user.save();
+
+    return res.status(201).json(_.pick(response, ["email", "score"]));
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = {
+  register,
+};
