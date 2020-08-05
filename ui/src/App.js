8f5141fc8132551fee1507b78b0e6dcc328c265a
Lazar Karic <kadzajr@gmail.com>
2020-08-05 19:00:38 +0200

    Refactored layout to wrapper component, lazy loaded pages refactored routes

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,34 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-import Header from "./components/Header/Header";
-import Routes from "./components/Routes/Routes";
-import Footer from "./components/Footer/Footer";
+// Layout will always be loaded so I wont include it in lazy loading.
+import Layout from "./components/Layout/Layout";
+
+// Lazy loading pages
+const Home = lazy(() => import("./pages/Home"));
+const Quizzes = lazy(() => import("./pages/Quizzes"));
+const Ladder = lazy(() => import("./pages/Ladder"));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <Footer />
-    </BrowserRouter>
+    <Router>
+      <Layout>
+        {/* Loading hardcoded untill we get spinner/loader task done */}
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/quizzes" component={Quizzes} />
+            <Route path="/ladder" component={Ladder} />
+            <Route render={() => <Redirect to="/" />} />
+          </Switch>
+        </Suspense>
+      </Layout>
+    </Router>
   );
 }
 
--- ui/src/components/Layout/Layout.js
+++ ui/src/components/Layout/Layout.js
@@ -1,7 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Layout = () => {
-  return <div>Hello from Layout</div>;
+// Components
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node,
 };
 
 export default Layout;
