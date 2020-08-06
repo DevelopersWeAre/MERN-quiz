7d58c6fc76280ebe47c79f894818cde94446439a
Lazar Karic <kadzajr@gmail.com>
2020-08-06 23:14:13 +0200

    Added interceptor todo

--- ui/src/utils/axios.js
+++ ui/src/utils/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+// TODO - Add the response interceptors (error handling).
+
 // This is base url, in the case that there is a value in env it takes that, otherwise it takes the string on the right.
 const BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api/v1";
@@ -16,7 +18,7 @@ AUTH_API.interceptors.request.use(
   config => {
     let token = localStorage.getItem(ACCESS_TOKEN);
 
-    config.headers = { Authorization: `Bearer ${token}` };
+    config.headers = { ...config.headers, Authorization: `Bearer ${token}` };
 
     return config;
   },
